20bdeaf73a78e71b1a22a1066df6931ef25a35e9
穗乃果 <1763575579@qq.com>
2021-05-12 09:36:13 +0800

    更新客户端骨骼

--- Overlay.cs
+++ Overlay.cs
@@ -126,35 +126,35 @@ namespace CSGO_External_Overlay
 
             for (int i = 1; i <= player_count; i++)
             {
-                int entity = Memory.ReadMemory<int>(csgo.server + 0xA7F7E4 + i * 0x18);
+                int server_entity = Memory.ReadMemory<int>(csgo.server + 0xA7F7E4 + i * 0x18);
 
-                int entity_health = Memory.ReadMemory<int>(entity + 0x230);
-                if (entity_health <= 0) continue;
-                int entity_team = Memory.ReadMemory<int>(entity + 0x314);
-                if (entity_team == player_team) continue;
+                int server_entity_health = Memory.ReadMemory<int>(server_entity + 0x230);
+                if (server_entity_health <= 0) continue;
+                int server_entity_team = Memory.ReadMemory<int>(server_entity + 0x314);
+                if (server_entity_team == player_team) continue;
 
-                Vector3 v3PlayerPos = new Vector3
+                Vector3 server_v3PlayerPos = new Vector3
                 {
-                    X = Memory.ReadMemory<float>(entity + 0x1DC),
-                    Y = Memory.ReadMemory<float>(entity + 0x1DC + 0x4),
-                    Z = Memory.ReadMemory<float>(entity + 0x1DC + 0x8)
+                    X = Memory.ReadMemory<float>(server_entity + 0x1DC),
+                    Y = Memory.ReadMemory<float>(server_entity + 0x1DC + 0x4),
+                    Z = Memory.ReadMemory<float>(server_entity + 0x1DC + 0x8)
                 };
 
-                Vector2 v2PlayerPos = WorldToScreen(v3PlayerPos);
-                Vector2 v2BoxWH = GetBoxWH(gfx, v3PlayerPos, 75.0f, 0.0f);
+                Vector2 server_v2PlayerPos = WorldToScreen(server_v3PlayerPos);
+                Vector2 server_v2BoxWH = GetBoxWH(gfx, server_v3PlayerPos, 75.0f, 0.0f);
 
-                if (!IsNullVector2(v2PlayerPos))
+                if (!IsNullVector2(server_v2PlayerPos))
                 {
-                    float box_height = v2BoxWH.X;
-                    float box_wight = v2BoxWH.Y;
+                    float box_height = server_v2BoxWH.X;
+                    float box_wight = server_v2BoxWH.Y;
 
                     // 测试
                     //gfx.DrawText(_fonts["Microsoft YaHei"], 10, _brushes["red"], v2PedPos.X, v2PedPos.Y, $"#");
 
                     // 2D方框
                     gfx.DrawRectangle(_brushes["white"], Rectangle.Create(
-                        v2PlayerPos.X - box_wight / 2,
-                        v2PlayerPos.Y - box_height,
+                        server_v2PlayerPos.X - box_wight / 2,
+                        server_v2PlayerPos.Y - box_height,
                         box_wight,
                         box_height), 0.7f);
 
@@ -162,34 +162,35 @@ namespace CSGO_External_Overlay
                     gfx.DrawLine(_brushes["white"],
                         csgo.windowData.Width / 2,
                         0,
-                        v2PlayerPos.X,
-                        v2PlayerPos.Y - box_height, 0.7f);
+                        server_v2PlayerPos.X,
+                        server_v2PlayerPos.Y - box_height, 0.7f);
 
                     // 血条
                     gfx.DrawRectangle(_brushes["white"], Rectangle.Create(
-                        v2PlayerPos.X - box_wight / 2 - box_wight / 8,
-                        v2PlayerPos.Y,
+                        server_v2PlayerPos.X - box_wight / 2 - box_wight / 8,
+                        server_v2PlayerPos.Y,
                         box_wight / 10,
                         box_height * -1.0f), 0.7f);
                     gfx.FillRectangle(_brushes["green"], Rectangle.Create(
-                        v2PlayerPos.X - box_wight / 2 - box_wight / 8,
-                        v2PlayerPos.Y,
+                        server_v2PlayerPos.X - box_wight / 2 - box_wight / 8,
+                        server_v2PlayerPos.Y,
                         box_wight / 10,
-                        box_height * entity_health / 100 * -1.0f));
+                        box_height * server_entity_health / 100 * -1.0f));
 
                     // 血量
                     gfx.DrawText(_fonts["Microsoft YaHei"], 8, _brushes["white"],
-                        v2PlayerPos.X - box_wight / 2,
-                        v2PlayerPos.Y + box_wight / 8 - box_wight / 10,
-                        $"HP: {entity_health:0}/{100}\n" +
+                        server_v2PlayerPos.X - box_wight / 2,
+                        server_v2PlayerPos.Y + box_wight / 8 - box_wight / 10,
+                        $"HP: {server_entity_health:0}/{100}\n" +
                         $"ID: {i}");
                 }
 
-                //Vector2 v2Bone0 = WorldToScreen(GetBonePosition(entity, 8));
-                //if (!IsNullVector2(v2Bone0))
-                //{
-                //    gfx.DrawCircle(_brushes["red"], v2Bone0.X, v2Bone0.Y, 5, 1);
-                //}
+                int client_entity = Memory.ReadMemory<int>(csgo.client + Offsets.signatures.dwEntityList + i * 0x10);
+
+                int client_entity_health = Memory.ReadMemory<int>(client_entity + Offsets.netvars.m_iHealth);
+                if (client_entity_health <= 0) continue;
+                int client_entity_team = Memory.ReadMemory<int>(client_entity + Offsets.netvars.m_iTeamNum);
+                if (client_entity_team == player_team) continue;
 
                 //for (int j = 0; j < 80; j++)
                 //{
@@ -200,34 +201,34 @@ namespace CSGO_External_Overlay
                 // buggly | https://www.unknowncheats.me/forum/counterstrike-global-offensive/218227-skeleton-esp-bone-ids.html
                 // skeleton esp | Bone Ids : https://www.unknowncheats.me/forum/attachments/counterstrike-global-offensive/13413d1480413236-csgo-bone-id-8f0bb9a93378477388dee312b2fad4ca-png
 
-                //Vector2 v2Bone1 = WorldToScreen(GetBonePosition(entity, 2));
-                //Vector2 v2Bone0 = WorldToScreen(GetBonePosition(entity, 1));
+                Vector2 v2Bone1 = WorldToScreen(GetBonePosition(client_entity, 2));
+                Vector2 v2Bone0 = WorldToScreen(GetBonePosition(client_entity, 1));
 
-                //if (!IsNullVector2(v2Bone0) && !IsNullVector2(v2Bone1))
-                //{
-                //    float BoxHeight = v2Bone0.Y - v2Bone1.Y;
-                //    float BoxWidth = BoxHeight / 2;
+                if (!IsNullVector2(v2Bone0) && !IsNullVector2(v2Bone1))
+                {
+                    float BoxHeight = v2Bone0.Y - v2Bone1.Y;
+                    float BoxWidth = BoxHeight / 2;
 
-                //    gfx.DrawRectangle(_brushes["red"], v2Bone0.X - BoxWidth / 2, v2Bone0.Y, v2Bone0.X - BoxWidth / 2 + BoxWidth, v2Bone0.Y - BoxHeight, 1);
-                //}
+                    gfx.DrawRectangle(_brushes["red"], v2Bone0.X - BoxWidth / 2, v2Bone0.Y, v2Bone0.X - BoxWidth / 2 + BoxWidth, v2Bone0.Y - BoxHeight, 1);
+                }
 
-                //DrawBone(gfx, 8, 7, entity);
+                DrawBone(gfx, 8, 7, client_entity);
 
-                //DrawBone(gfx, 7, 40, entity);
-                //DrawBone(gfx, 40, 41, entity);
+                DrawBone(gfx, 7, 40, client_entity);
+                DrawBone(gfx, 40, 41, client_entity);
 
-                //DrawBone(gfx, 7, 12, entity);
-                //DrawBone(gfx, 12, 13, entity);
+                DrawBone(gfx, 7, 12, client_entity);
+                DrawBone(gfx, 12, 13, client_entity);
 
-                //DrawBone(gfx, 7, 6, entity);
-                //DrawBone(gfx, 6, 4, entity);
-                //DrawBone(gfx, 4, 3, entity);
+                DrawBone(gfx, 7, 6, client_entity);
+                DrawBone(gfx, 6, 4, client_entity);
+                DrawBone(gfx, 4, 3, client_entity);
 
-                //DrawBone(gfx, 3, 74, entity);
-                //DrawBone(gfx, 74, 75, entity);
+                DrawBone(gfx, 3, 74, client_entity);
+                DrawBone(gfx, 74, 75, client_entity);
 
-                //DrawBone(gfx, 3, 67, entity);
-                //DrawBone(gfx, 67, 68, entity);
+                DrawBone(gfx, 3, 67, client_entity);
+                DrawBone(gfx, 67, 68, client_entity);
             }
         }
 
